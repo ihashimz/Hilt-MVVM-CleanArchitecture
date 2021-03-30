8e417d293249b76f05bb23868e9ad782454740f6
ihashimz <ihashimz@yahoo.com>
2021-03-30 12:57:13 +0300

    Removing the unused srevices and refactor names

--- app/src/main/java/com/example/hilt_mvvm/ui/main/data/repository/PostRepository.kt
+++ app/src/main/java/com/example/hilt_mvvm/ui/main/data/repository/PostRepository.kt
@@ -1,33 +1,15 @@
 package com.example.hilt_mvvm.ui.main.data.repository
 
 import androidx.lifecycle.MutableLiveData
-import com.example.hilt_mvvm.core.network.BaseRepository
 import com.example.hilt_mvvm.core.network.BaseResponsesRepository
 import com.example.hilt_mvvm.core.network.Resource
 import com.example.hilt_mvvm.core.network.ServiceGenerator
 import com.example.hilt_mvvm.ui.main.data.entities.Post
 import com.example.hilt_mvvm.ui.main.data.service.PostsServices
-import com.google.gson.Gson
 
 val postsServices = ServiceGenerator().createService(PostsServices::class.java)
 
 class PostRepository {
-
-    suspend fun getPosts(): MutableLiveData<Resource<ArrayList<Post>>> {
-        val mutableLiveData = MutableLiveData<Resource<ArrayList<Post>?>>()
-        mutableLiveData.value = Resource.loading(null)
-        val req = postsServices!!.getPosts()
-        return BaseRepository<ArrayList<Post>>(req)
-    }
-
-
-    // Old Way
-    suspend fun getMutablePost(id: Int): MutableLiveData<Resource<Post>>{
-        val mutableLiveData = MutableLiveData<Resource<Post>?>()
-        mutableLiveData.value = Resource.loading(null)
-        val req = postsServices!!.getMutablePost(id)
-        return BaseRepository<Post>(req)
-    }
 
 
     suspend fun getPost(id: Int) = postsServices.getPost(id)
